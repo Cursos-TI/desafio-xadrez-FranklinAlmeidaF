f1765e2822a09f6f843ad9ddb54e85b8b0c80895
FranklinAlmeidaF <franklin.a.felix@gmail.com>
2025-03-12 23:21:43 +0000

    aventureiro

--- Xadrez2.c
+++ Xadrez2.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
+void moverCavalo(int linha, int coluna) {
+    int movimentos[8][2] = {
+        {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+        {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+    };
+
+    printf("Movimentos possíveis do Cavalo:\n");
+    for (int i = 0; i < 8; i++) {
+        int novaLinha = linha + movimentos[i][0];
+        int novaColuna = coluna + movimentos[i][1];
+        if (novaLinha >= 0 && novaLinha < 8 && novaColuna >= 0 && novaColuna < 8) {
+            printf("Linha: %d, Coluna: %d\n", novaLinha, novaColuna);
+        }
+    }
+}
+
 int main() {
-    // Definindo as variáveis para o movimento do Cavalo
-    int casas_para_baixo = 2;  // O Cavalo se move duas casas para baixo
-    int casas_para_esquerda = 1;  // E uma casa para a esquerda
-
-    // Movimento do Cavalo
-    printf("Movimento do Cavalo:\n");
-
-    // Loop para mover duas casas para baixo
-    for (int i = 0; i < casas_para_baixo; i++) {
-        printf("Baixo\n");
-    }
-
-    // Loop para mover uma casa para a esquerda
-    int contador = 0;
-    while (contador < casas_para_esquerda) {
-        printf("Esquerda\n");
-        contador++;
-    }
-
-    // Linha em branco para separar o movimento do Cavalo de outros movimentos
-    printf("\n");
-
-    // Exemplo de movimento de outras peças (opcional)
-    printf("Movimento de outras peças:\n");
-    printf("Cima\n");
-    printf("Direita\n");
-
+    moverCavalo(4, 4);
     return 0;
 }
